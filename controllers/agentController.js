f715b467cca96b79fa54ce993313c3e606e2af4c
DivineDemon <supame123@gmail.com>
2023-06-15 11:43:41 +0500

    Get One Agent! ✨🚀

--- controllers/agentController.js
+++ controllers/agentController.js
@@ -27,6 +27,35 @@ export const addAgent = async (req, res) => {
   }
 };
 
+export const getAgent = async (req, res) => {
+  try {
+    const response = await prisma.agent.findUnique({
+      where: {
+        id: req.params.agent_id,
+      },
+    });
+
+    if (response.length <= 0) {
+      res.status(404).json({
+        success: false,
+        message: "Agent Not Found!",
+      });
+    } else {
+      res.status(200).json({
+        status: true,
+        message: "Retrieved Agent!",
+        response,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Please Try Again!",
+      error: error.message,
+    });
+  }
+};
+
 export const getAllAgents = async (_, res) => {
   try {
     const response = await prisma.agent.findMany();
--- routes/agentRoute.js
+++ routes/agentRoute.js
@@ -1,8 +1,13 @@
 import { Router } from "express";
-import { addAgent, getAllAgents } from "../controllers/agentController.js";
+import {
+  addAgent,
+  getAgent,
+  getAllAgents,
+} from "../controllers/agentController.js";
 
 const router = new Router();
 
 router.route("/").get(getAllAgents).post(addAgent);
+router.get("/:agent_id", getAgent);
 
 export default router;
